a9f40a75350ea84d34bd08c0c0fa300f5be49063
Afroz Alam <afrozalam01212@gmail.com>
2026-07-29 12:14:41 +0530

    Add files via upload

---
+++ resturent/resturent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resturent
+//
+//  Created by Afroz on 08/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ resturent/resturent/biryanibucketViewController.swift
@@ -0,0 +1,29 @@
+//
+//  biryanibucketViewController.swift
+//  resturent
+//
+//  Created by Afroz on 08/09/25.
+//
+
+import UIKit
+
+class biryanibucketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
